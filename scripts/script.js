f4b272207e4bb147873c827bcf5900158bc02834
Shatskikh Andrey <ponchic-09@mail.ru>
2023-03-10 23:28:38 +0300

    Feat: Js is workinggit add -A!

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,33 @@
-let editButton = document.querySelector('.profile__edit-button');
+let buttonEdit = document.querySelector('.profile__edit-button');
+let formElement = document.querySelector('.popup__form'); 
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__job');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let saveButton = formElement.querySelector('.popup__submit');
+
 function openPopup() {
     let popup = document.querySelector('.popup');
     popup.classList.remove('popup__closed');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
 }
 
-editButton.addEventListener('click', openPopup);
+buttonEdit.addEventListener('click', openPopup);
 
-let closeButton = document.querySelector('.popup__close');
+let buttonClose = document.querySelector('.popup__close');
 function closePopup() {
     let popup = document.querySelector('.popup');
     popup.classList.add('popup__closed');
 }
 
-closeButton.addEventListener('click', closePopup);
+buttonClose.addEventListener('click', closePopup);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = jobInput.value;
+    closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
